8b1cfe6c1c46af43db0eecefc3af6f8ac6d377a5
Magnus Karström <magnus@yesweb.se>
2022-08-22 08:45:47 +0200

    fix All: Migrating to folderbased routing

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,5 +1,5 @@
-<nav class="flex justify-center w-full">
-    <a class="mx-4 text-lg" href="/">Home</a>
-    <a class="mx-4 text-lg" href="/about">About</a>
+<nav class="flex justify-center w-full h-3">
+	<a class="mx-4 text-lg" href="/">Hem</a>
+	<a class="mx-4 text-lg" href="/search">Sök</a>
+	<a class="mx-4 text-lg" href="/about">Kontakt</a>
 </nav>
-
